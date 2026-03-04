5f514fe1effcde1b853262bb7a2594fa65307975
mdshamshad80635-netizen <mdshamshad80635@gmail.com>
2026-03-05 04:13:47 +0530

    Create Home component for ZetsyBuy landing page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>ZetsyBuy</h1>
+      <p>Gift Store + Amazon Affiliate Website</p>
+      <a href="https://wa.me/919999999999">Order on WhatsApp</a>
+    </div>
+  );
+}
